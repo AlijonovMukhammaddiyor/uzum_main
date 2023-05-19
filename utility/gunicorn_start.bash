1f5803a68798681bf1ef69ba5917609ec8dbc59d
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-05-19 19:32:51 +0900

    change to production.py

---
+++ utility/gunicorn_start.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NAME="uzum"                                  							              # Name of the application
+DJANGODIR=/home/developer/uzum             				        # Django project directory
+DJANGOENVDIR=/home/developer/uzum/uzum_env            			    # Django project env
+SOCKFILE=/home/developer/uzum/run/gunicorn.sock  		  # we will communicte using this unix socket
+USER=developer                                        					              # the user to run as
+GROUP=developer                                     							            # the group to run as
+NUM_WORKERS=3                                    							            # how many worker processes should Gunicorn spawn (2 * CPUs + 1)
+DJANGO_SETTINGS_MODULE=uzum.config.settings.production         						            # which settings file should Django use
+DJANGO_WSGI_MODULE=uzum.config.wsgi                     						            # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source /home/developer/uzum/uzum_env/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec ${DJANGOENVDIR}/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
