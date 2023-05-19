4f2072c8d081a1fe4619f0f97fe4b59f2d2c566b
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-05-20 08:42:48 +0900

    change celery jobs to async

--- config/settings/base.py
+++ config/settings/base.py
@@ -342,7 +342,7 @@ SPECTACULAR_SETTINGS = {
 CELERY_BEAT_SCHEDULE = {
     "update_data": {
         "task": "update_uzum_data",
-        "schedule": crontab(minute=10, hour=4, day_of_week="*"),
+        "schedule": crontab(minute=44, hour=4, day_of_week="*"),
         "args": (),
     },
 }
