6376f147ee7788e0b685147660dcc9871bc1bebf
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-05-21 22:15:42 +0900

    set celery time to 00:05 everyday

--- config/settings/base.py
+++ config/settings/base.py
@@ -342,7 +342,7 @@ SPECTACULAR_SETTINGS = {
 CELERY_BEAT_SCHEDULE = {
     "update_data": {
         "task": "update_uzum_data",
-        "schedule": crontab(minute=22, hour=5, day_of_week="*"),
+        "schedule": crontab(minute=5, hour=0, day_of_week="*"),
         "args": (),
     },
 }
