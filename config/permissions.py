a23d035fbbc4d59bf9f06fa4fabf02e0a83595e6
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-06-13 23:57:57 +0900

    test isAdminOrDeveloper

--- config/permissions.py
+++ config/permissions.py
@@ -3,14 +3,17 @@ from rest_framework import permissions
 
 class IsAdminOrDeveloper(permissions.BasePermission):
     def has_permission(self, request, view):
-        print(request.user.is_superuser, request.user.is_developer)
-        # Check if the user is an admin.
-        if request.user.is_superuser:
-            return True
+        try:
+            print(request.user.is_superuser, request.user.is_developer)
+            # Check if the user is a developer.
+            if request.user.is_developer:
+                return True
 
-        # Check if the user is a developer.
-        if request.user.is_developer:
-            return True
+            if request.user.is_superuser:
+                return True
 
-        # If neither of the above, deny permission.
-        return False
+            # If neither of the above, deny permission.
+            return False
+        except AttributeError as e:
+            print("AttributeError in IsAdminOrDeveloper: ", e)
+            return False
