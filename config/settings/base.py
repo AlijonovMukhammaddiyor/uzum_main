3cec27d2d543b00492c925dd3b8140098abf6a79
Alijonov Mukhammaddiyor <alijonov.md@gmail.com>
2023-06-20 05:18:50 +0000

    save

--- config/settings/base.py
+++ config/settings/base.py
@@ -356,7 +356,7 @@ SPECTACULAR_SETTINGS = {
 CELERY_BEAT_SCHEDULE = {
     "update_data": {
         "task": "update_uzum_data",
-        "schedule": crontab(minute=0, hour=0, day_of_week="*"),
+        "schedule": crontab(minute=20, hour=10, day_of_week="*"),
         "args": (),
     },
 }
