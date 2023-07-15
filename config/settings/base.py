dbd7bde6f16ef23beb8ee99f1f2aa1ae2aec9f5f
Alijonov Mukhammaddiyor <alijonov.md@gmail.com>
2023-07-15 08:38:35 +0000

    change job time

--- config/settings/base.py
+++ config/settings/base.py
@@ -364,7 +364,7 @@ SPECTACULAR_SETTINGS = {
 CELERY_BEAT_SCHEDULE = {
     "update_data": {
         "task": "update_uzum_data",
-        "schedule": crontab(minute=30, hour=2, day_of_week="*"),
+        "schedule": crontab(minute=5, hour=0, day_of_week="*"),
         "args": (),
     },
 }
