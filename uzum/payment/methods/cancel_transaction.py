1991da6d2cd9da216d1aa16ea9854a772e200c0c
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-08-12 16:32:28 +0900

    add payme

--- uzum/payment/methods/cancel_transaction.py
+++ uzum/payment/methods/cancel_transaction.py
@@ -22,11 +22,11 @@ class CancelTransaction:
 
     @transaction.atomic
     def __call__(self, params: dict):
-        clean_data: dict = MTMS.get_validated_data(params=params)
+        # clean_data: dict = MTMS.get_validated_data(params=params)
         try:
             with transaction.atomic():
                 transactions: MerchatTransactionsModel = MerchatTransactionsModel.objects.filter(
-                    _id=clean_data.get("_id"),
+                    _id=params.get("id"),
                 ).first()
                 if transactions.cancel_time == 0:
                     transactions.cancel_time = int(time.time() * 1000)
@@ -34,7 +34,7 @@ class CancelTransaction:
                     transactions.state = -1
                 if transactions.perform_time != 0:
                     transactions.state = -2
-                transactions.reason = clean_data.get("reason")
+                transactions.reason = params.get("reason")
                 transactions.save()
 
         except PerformTransactionDoesNotExist as error:
