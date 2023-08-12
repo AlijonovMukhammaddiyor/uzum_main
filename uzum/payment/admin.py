1314838aa42a5d6c1bff0f909bab452c9c95e70e
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-08-12 12:25:36 +0900

    add payme

--- uzum/payment/admin.py
+++ uzum/payment/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 
-from uzum.payment.models import MerchatTransactionsModel
+from uzum.payment.models import MerchatTransactionsModel, Order
 
 
 @admin.register(MerchatTransactionsModel)
 class MerchatTransactionsModelAdmin(admin.ModelAdmin):
+    # list_display = ("user", "timestamp", "amount", "payment_method", "payment_id")
+    # search_fields = ("user__username", "user__email")
+    # list_filter = ("payment_method",)
+    # ordering = ("-timestamp",)
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
     # list_display = ("user", "timestamp", "amount", "payment_method", "payment_id")
     # search_fields = ("user__username", "user__email")
     # list_filter = ("payment_method",)
